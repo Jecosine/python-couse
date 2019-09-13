d067979b6d511dba52cb419ab0d37ca66edc63e8
Blacks <mj_ol@outlook.com>
2019-09-13 16:39:22 +0800

    UPDATE: Add comments

--- image_watermark1.py
+++ image_watermark1.py
@@ -2,6 +2,8 @@ from PIL import Image, ImageDraw, ImageFont
 import myutils as ut
 import os
 '''
+add_watermarks adds the image-type logo to origin images in specified directory
+
 @para path: <string> select a directory to process all jpg
 @para logopath: <string> select a image's path which used as water mark
 @para logo_size: <list> a list of length 2 to describe the logo size (width, height)
@@ -17,19 +19,22 @@ def add_watermarks(path, logopath, logo_size = [200, 100], position = [0, 0]):
             out = add_watermark_single(im, logo, logo_size, position)
             out.convert('RGB').save(fname)
         except Exception as e:
-            print "Cannot convert file {0}".format(i), e
+            print "Cannot convert file.Error info: {0}".format(i), e
         else:
             print "{0} coverted".format(i)
 
 '''
+logo_resize resize the logo image to fit the image size
+
 @para logo: <class 'PIL.PngImagePlugin.PngImageFile'> image used as water mark
 @para logo_size: <list> a list of length 2 to describe the logo size (width, height)
 '''
 def logo_resize(logo, logo_size):
     return logo.resize(logo_size)
 
-
 '''
+add_watermark_single add water mark to one image
+
 @para im: <class 'PIL.PngImagePlugin.PngImageFile'> origin image
 @para logo: <class 'PIL.PngImagePlugin.PngImageFile'> image used as water mark
 @para position: <list> a list with length 2
@@ -40,5 +45,7 @@ def add_watermark_single(im, logo, logo_size, position):
     panel.paste(logo, position)
     output = Image.alpha_composite(im, panel)
     return output
+
+# Example
 if __name__ == "__main__":
     add_watermarks(".", "python-logo.png", [200, 100], [0, 0])
--- image_watermark2.py
+++ image_watermark2.py
@@ -1,15 +1,28 @@
 from PIL import Image, ImageDraw, ImageFont
 import myutils as ut
 import os
-def add_watermarks(path):
+
+'''
+@para path: <string> select a directory to process all jpg
+@para content: <string> content of watermark
+@para position: <list> a list with length 2
+@para color: <list> a list with length 4
+@para fpath: <string> the path of font
+'''
+def add_watermarks(path, content = 'Python', position = [0, 0], color = [255,255,255,100], fpath = "LucidaTypewriterRegular.ttf"):
+    # Get image list of specified directory
     imlist = ut.get_imlist(path, ".jpg")
     for i in imlist:
         fname = os.path.splitext(i)[0] + '_w.jpg'
         im = Image.open(i).convert('RGBA')
-        out = add_watermark_single(im, "Python", [0, 0], [255,255,255,100], "LucidaTypewriterRegular.ttf")
-        out.convert("RGB").save(fname)
-
-
+        try: 
+            out = add_watermark_single(im, content, position, color, fpath)
+            #out is RGBA after alpha composite with watermark
+            out.convert("RGB").save(fname)
+        except Exception as e:
+            print "Cannot add water mark to {0}, due to error(s): {1}".format(i, e)
+        else:
+            print "Water mark added to {1}".format(i)
 '''
 @para im: <class 'PIL.PngImagePlugin.PngImageFile'> origin image
 @para content: <string> content of watermark
