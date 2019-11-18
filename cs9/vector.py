cd61a928e8b6bcd311c56628145483cf697075d8
silver <mj_ol@outlook.com>
2019-11-18 15:30:56 +0900

    UPDATE:Update

--- cs9/vector.py
+++ cs9/vector.py
@@ -1,15 +1,17 @@
-import random
+import random, math
 
 class Vector:
     def __init__(self, l):
         self.__value = l
         self.__length = len(l)
     def __getitem__(self, i):
-        return self.l[i]
+        return self.__value[i]
     def length(self):
         return self.length
+    def scale(self, n):
+        return Vector([i * n for i in self.__value])
     def __add__(self, b):
-        result = [self.__value[i] + b[i] for i in range(self.__length)]s
+        result = [self.__value[i] + b[i] for i in range(self.__length)]
         return Vector(result)
     def __sub__(self, b):
         c = [-i for i in b]
@@ -17,16 +19,23 @@ class Vector:
     def __abs__(self):
         return math.sqrt(self.dot(self))
     def dot(self, b):
-        result = [self.__value[i] + b[i] for i in range(self.__length)]
+        result = [self.__value[i] * b[i] for i in range(self.__length)]
         return sum(result)
-    
     def normalize(self):
         return Vector([i / self.__abs__() for i in self.__value])
-    
     def __len__(self):
         return self.__length
     def __str__(self):
         return str(self.__value)
 
-class Sketch:
+class sketch
+
+if __name__ == "__main__":
+    v1 = Vector((1,2))
+    v2 = Vector((2,3))
+    print("length of v1 is %s" % len(v1))
+    print("v1 + v2 = %s" % (v1+v2))
+    print("v1 · v2 = %s" % v1.dot(v2))
+    print("|v1| = %s" %abs(v1))
+    print("2v1 = %s" % v1.scale(2))
     
