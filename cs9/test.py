a06650457b0fe848fc1d89416f852005c5df6609
silver <mj_ol@outlook.com>
2019-11-19 09:34:29 +0900

    FINISH:Finish cs9

--- cs9/test.py
+++ cs9/test.py
@@ -1,5 +1,16 @@
 from vector import *
 import os
+import random
+def generate():
+    # add a-z and A-Z
+    l = [chr(i) for i in range(65, 65 + 26)]
+    l += [chr(i) for i in range(97, 97 + 26)]
+    # add space and comma and dot, increase space count to improve proprability
+    l += [' ', ' ', ' ', ' ', ',', '.']
+    s = ''
+    for i in range(400):
+        s += random.choice(l)
+    return s
 
 def compare_file(path):
     # get txt file
@@ -17,7 +28,7 @@ def compare_file(path):
             if not d.get(t):
                 with open(path + '/' + t, 'r') as f:
                     content = f.read()
-                d[t] = Sketch(content, 5, 100)
+                d[t] = Sketch(content, 5, 2000)
             s = d[text]
             t = d[t]
             print("{:.6f}".format(s.similarity(t)).center(15), end="|")
