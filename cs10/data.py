4ddabc3b47df8c8cc01c56da87682259545553b3
silver <mj_ol@outlook.com>
2019-12-10 14:12:25 +0900

    UPDATE:Huffman Python

--- cs10/data.py
+++ cs10/data.py
@@ -1,12 +1,10 @@
-import os 
-import json 
- #全局变量 _products = {} 
- #保存产品信息的字典：sku_id:sku_name _locations = {}
- #保存货架位置的字典：loc_id:loc_name _items = [] 
- #保存商品库存的列表，元素为元组(sku_id,loc_id) 
+import os, json 
+_products = {} 
+_locations = {}
+_items = [] 
  
 def init(): 
-    """从磁盘JSON格式文件中读取数据""" 
+    # read from json 
     global _products, _locations, _items 
     if os.path.exists("products.json"): 
         f = open("products.json", "r", encoding='utf-8') 
@@ -22,61 +20,58 @@ def init():
         f.close() 
          
 def _save_products(): 
-    """ 把产品信息数据_products以JSON格式保存到磁盘文件""" 
+    # save products in json
     global _products 
     f = open("products.json", "w", encoding='utf-8') 
     f.write(json.dumps(_products, ensure_ascii=False)) 
     f.close() 
  
 def _save_locations(): 
-    """ 把货架位置数据_locations以JSON格式保存到磁盘文件""" 
+    # save location in json
     global _locations 
     f = open("locations.json", "w", encoding='utf-8') 
     f.write(json.dumps(_locations)) 
     f.close() 
  
 def _save_items(): 
-    """ 把商品库存数据_items以JSON格式保存到磁盘文件""" 
+    # save item in json
     global _items 
     f = open("items.json", "w", encoding='utf-8') 
     f.write(json.dumps(_items)) 
     f.close() 
      
-def get_products(): 
-    """ 返回产品信息 """ 
+def get_products():  
     global _products 
     return _products 
  
 def get_locations(): 
-    """ 返回货架位置信息 """ 
     global _locations 
     return _locations 
  
 def get_items(): 
-    """ 返回商品库存信息 """ 
     global _items 
     return _items 
  
 def add_product(sku_id, sku_name): 
-    """ 增加一个产品sku_id、sku_name """ 
+    # add procuct
     global _products 
     _products[sku_id] = sku_name 
     _save_products() 
  
 def add_location(loc_id, loc_name): 
-    """ 增加一个货架位置loc_id、loc_name """ 
+    # add location
     global _locations 
     _locations[loc_id] = loc_name 
     _save_locations() 
  
 def add_item(sku_id, loc_id): 
-    """ 入库一件商品：商品sku_id、货架sku_id """ 
+    # add an item
     global _items 
     _items.append((sku_id, loc_id)) 
     _save_items() 
  
 def remove_item(sku_id, loc_id): 
-    """出库一件商品：商品sku_id、货架sku_id，返回True；如果不存在，返回False""" 
+    # pop an item
     global _items 
     for i in range(len(_items)): 
         if sku_id == _items[i][0] and loc_id == _items[i][1]: 
